99dc53d232f0ed75f79f6ca0a42db59965371040
ShaikhMasud <shaikhmasud507@gmail.com>
2024-09-01 21:43:52 +0530

    first commit

--- cleint/src/App.js
+++ cleint/src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Login/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
         <Route path='/addteachers' element={<AddTeacher/>}></Route>
