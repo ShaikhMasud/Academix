d2b268b807d537a8a7a7f678656393b528ea4870
ShaikhMasud <shaikhmasud507@gmail.com>
2024-09-14 13:44:52 +0530

    1 and 2 page of hod

--- cleint/src/App.js
+++ cleint/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Login from './Login.js';
 import Home from './home.js';
 import AddTeacher from './AddTeacher.js';
+import Hodmainpage from './HOD/1.main pg hod/mainhodpag.js';
+import Subjectpage from './HOD/2.subj hod/subject.js';
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
         <Route path='/addteachers' element={<AddTeacher/>}></Route>
+        <Route path='/hoddash' element={<Hodmainpage/>}></Route>
+        <Route path='/sub' element={<Subjectpage/>}></Route>
       </Routes>
     </BrowserRouter>
   );
--- server/models/Users.js
+++ server/models/Users.js
@@ -18,6 +18,12 @@ const UserSchema = new mongoose.Schema({
         required:true,
     },
 })
+const TeacherSchema= new mongoose.Schema({
+    name : String,
+    email : String,
+    password : String,
+})
+
 
 const UserModel = mongoose.model("users",UserSchema)
 module.exports = UserModel
